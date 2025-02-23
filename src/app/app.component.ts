8517be132db3d67c581b4650ac13aad1402e7421
Mariusz Fik <mariusz@fidano.pl>
2025-02-23 22:00:56 +0100

    Hook up sakai-ng template.
    Signed-off-by: Mariusz Fik <mariusz@fidano.pl>

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,59 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {
+  Component,
+  computed,
+  HostBinding,
+  inject,
+  Renderer2,
+  signal,
+} from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
+import { ButtonModule } from 'primeng/button';
+import { InputTextModule } from 'primeng/inputtext';
+import { MessageModule } from 'primeng/message';
+import { ToggleButtonModule } from 'primeng/togglebutton';
+import { FluidModule } from 'primeng/fluid';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    InputTextModule,
+    ButtonModule,
+    MessageModule,
+    FormsModule,
+    ToggleButtonModule,
+    FluidModule,
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
+  standalone: true,
 })
 export class AppComponent {
-  title = 'plejlista357';
+  private renderer = inject(Renderer2);
+
+  isDarkModeEnabled = signal(false);
+  darkModeIcon = computed(() =>
+    this.isDarkModeEnabled() ? 'pi pi-sun' : 'pi pi-moon',
+  );
+
+  text = '';
+
+  msg = '';
+
+  onClick() {
+    this.msg = 'Welcome ' + this.text;
+  }
+
+  toggleTheme() {
+    if (this.isDarkModeEnabled()) {
+      this.renderer.removeClass(document.documentElement, 'app-dark');
+    } else {
+      this.renderer.addClass(document.documentElement, 'app-dark');
+    }
+
+    this.isDarkModeEnabled.update((mode) => !mode);
+  }
 }
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,21 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { provideRouter } from '@angular/router';
+import { providePrimeNG } from 'primeng/config';
 
 import { routes } from './app.routes';
+import { Emerald } from './app.theme';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideAnimationsAsync(),
+    providePrimeNG({
+      theme: {
+        preset: Emerald,
+        options: { darkModeSelector: '.app-dark' },
+      },
+    }),
+  ],
 };
