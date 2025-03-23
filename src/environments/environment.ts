73e591b2d6fccc07f58d82541bd524c256a892f9
Mariusz Fik <mariusz@fidano.pl>
2025-03-23 16:46:17 +0100

    Update redirectUrl.
    Signed-off-by: Mariusz Fik <mariusz@fidano.pl>

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   spotifyClientId: 'cfa5b908eb5a403e93146ff68e441360',
-  redirectUrl: 'https://fisiu.github.io/plejlista357/callback',
+  redirectUrl: 'https://plejlista357.vercel.app/callback',
 };
