c9fb0fbe07abd0fd692d6b4a3a37a3c0a24a8788
Mariusz Fik <mariusz@fidano.pl>
2026-01-22 14:18:45 +0100

    Update theme based on sakai 20, tailwind 4.
    Signed-off-by: Mariusz Fik <mariusz@fidano.pl>

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,7 +1,7 @@
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withEnabledBlockingInitialNavigation, withInMemoryScrolling } from '@angular/router';
 import { providePrimeNG } from 'primeng/config';
 import { routes } from './app.routes';
 import { Emerald } from './app.theme';
@@ -10,7 +10,11 @@ import { spotifyAuthInterceptor } from './services/spotify-auth.interceptor';
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes),
+    provideRouter(
+      routes,
+      withInMemoryScrolling({ anchorScrolling: 'enabled', scrollPositionRestoration: 'enabled' }),
+      withEnabledBlockingInitialNavigation(),
+    ),
     provideAnimationsAsync(),
     providePrimeNG({
       theme: {
